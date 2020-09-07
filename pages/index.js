887d4485b480bed064658cf006a765c6d7dc6238
Shashank Shekhar <shashank7200@gmail.com>
2020-09-07 15:48:57 +0530

    added resource list

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 class Index extends React.Component {
 	state = { open: false };
 	render() {
+		const emptyState = !store.get('ids');
 		return (
 			<Page>
 				<TitleBar
@@ -30,22 +31,25 @@ class Index extends React.Component {
 					onSelection={(resources) => this.handleSelection(resources)}
 					onCancel={() => this.setState({ open: false })}
 				/>
-				<Layout>
-					{/* <TextStyle variation="positive">
-						Sample app using React and Next.js
-					</TextStyle> */}
-					<EmptyState
-						heading="Discount your products temporarily"
-						action={{
-							content: 'Select products',
-							onAction: () => this.setState({ open: true }),
-						}}
-						image={img}
-					>
-						<p>Select products to change their price temporarily.</p>
-					</EmptyState>
-				</Layout>
-				<ResourceListWithProducts />
+				{ emptyState ? (
+					<Layout>
+						{/* <TextStyle variation="positive">
+							Sample app using React and Next.js
+						</TextStyle> */}
+						<EmptyState
+							heading="Discount your products temporarily"
+							action={{
+								content: 'Select products',
+								onAction: () => this.setState({ open: true }),
+							}}
+							image={img}
+						>
+							<p>Select products to change their price temporarily.</p>
+						</EmptyState>
+					</Layout>
+				) : (
+					<ResourceListWithProducts />
+				)}
 			</Page>
 		);
 	}
